cee18e7fefdf0baecaa2cc82d9f0a0d0f3003d37
Tanveen <racoctanveen15@gmail.com>
2024-08-04 00:12:09 +0600

    ok learning

---
+++ learning.js
@@ -0,0 +1,11 @@
+var math = 75.25;
+var bio = 65;
+var bang = 80;
+var chem = 35.45;
+var phy = 99.50;
+
+var sum = math + bio + bang + chem + phy;
+var gor = sum/5;
+
+gor = gor.toFixed(2);
+console.log(gor);
